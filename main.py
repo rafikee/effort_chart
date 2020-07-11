12972df9d2592f8f845781a2b531251562341d1c
Rafik Shoorbajee <rafik.vcu@gmail.com>
2020-07-11 02:36:36 -0400

    converted to json instead of csv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-import pandas as pd
+import json
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -9,12 +9,11 @@ app = Flask(__name__)
 # test deploy
 @app.route('/', methods=['GET'])
 def hello():
-    df = pd.read_csv('chart_input.csv', dtype=object)
-    stats = list(df['stats'].dropna())
-    cols = stats.copy()
-    #cols.insert(0, ' ')
-    players = ['#' + player for player in list(df['players'].dropna())]
-    return render_template('chart.html', plyrs=players, cols=cols, stats=stats)
+    with open('chart_input.json') as f:
+        data = json.load(f)
+    stats = data['stats']
+    players = data['players']
+    return render_template('chart.html', plyrs=players, stats=stats)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
